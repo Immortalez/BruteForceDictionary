7408e75e6a88db328bf62ea193476894992a03af
Tomasz <tm.gorol@gmail.com>
2019-01-19 21:14:19 +0100

    Moving to the other computer...

--- bfdictionary.py
+++ bfdictionary.py
@@ -1,7 +1,15 @@
+from math import factorial
 
-characters = "abcdefghijklmnopqrstuwxyz"
-length = 5
-combinations = len(characters) ** length  # Stores the amount of possible combinations
+# Taking necessary information form a user
+min_length = input("Minimal length: ")
+max_length = input("Maximal length: ")
+characters = "aaabbcccccdefghijklmnopqrstuwxyz"  # String with all the characters to be used
+# characters = "aaabbcccccdefghijklmnopqrstuwxyz"  # String with all the characters to be used
+
+# Eliminating duplicates
+char_set = sorted(list(set(characters)))
+
+combinations = len(characters) ** 5  # Stores the amount of possible combinations
 print("Possible combinations: {}".format(combinations))
 
 
